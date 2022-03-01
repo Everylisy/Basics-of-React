01f35d80aa271fdd600df1bacd30e74c802c3875
violetwhenisawu <anashape710@gmail.com>
2022-03-01 22:23:33 +0900

    Study about useEffect

--- jsx_basic/src/App.js
+++ jsx_basic/src/App.js
@@ -1,6 +1,33 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value); 
+
+  useEffect(() => {
+    console.log("CALL THE API... (한번만 실행되야 하는 코드)");
+  }, []);
+  useEffect(() => {
+    // if(keyword !== "" && keyword.length > 5) {
+    //   console.log("SEARCH FOR", keyword);
+    // }
+    console.log("검색창에 input이 들어왔을 때 실행되는 코드")
+  }, [keyword]);
+  useEffect(() => {
+    console.log("카운터가 변경될 때 실행되는 코드");
+  }, [counter]);
   return (
-    <div><h1>Welcome!</h1></div>
+    <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search Here..."/>
+      <h1 className={styles.title}>{counter}</h1>
+      <Button text={"Continue"}/>
+      <button onClick={onClick}>Click me~</button>
+    </div>
   );
 }
 
